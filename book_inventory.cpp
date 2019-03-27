dc0d1ff4b9d2ee90498c5149266a3dbe2ae11fcc
Beatrix <bealearnscode@gmail.com>
2019-03-27 14:39:34 +0000

    second commit

---
+++ book_inventory.cpp
@@ -0,0 +1,15 @@
+// Beatrix House
+// CPSC 230 - 10 AM
+// evanputten@live.esu.edu
+// April 2, 2019
+
+// The following program manages book inventory.
+// It completes the following 
+
+#include <iostream>
+#include <iomanip>
+
+addBook()
+lookUpBook()
+editBook()
+deleteBook()
